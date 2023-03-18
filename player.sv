fd091333cf4b8868e71a33726861faa79fab4b9d
Joaquin Philco <51135069+Joas329@users.noreply.github.com>
2023-03-17 22:11:04 -0400

    Update player.sv
    added reset

--- player.sv
+++ player.sv
@@ -3,6 +3,7 @@ module player(
 	input clk,
 	input button, button1,
 	input reg[15:0] x, y,
+	input reset,
 	output reg [15:0] x_player,
 	output reg [15:0] y_player
 	);
@@ -23,11 +24,31 @@ module player(
 	 state_t state, next_state;
 	 
 	  always @(posedge clk)begin
-		state<= next_state;
+		if(reset) state <= GROUND;
+		else state<= next_state;
 		end
 
 	reg [15:0] rx_player;
 	reg [15:0] ry_player;
+//	always @ (posedge clk_1ms)
+//	begin
+//		if(button)begin
+//			x_player <= x_player-1; // move to the left
+//		end
+//		if(button1)begin
+//			x_player <= x_player+1;	//move to the right
+//		end
+//		
+//		
+//		if(x_player <= 264 && x_player + 50 <= 314)  // left limit handeling
+//		begin
+//			x_player <= 265;
+//		end
+//		if( x_player+50 >= 664 && x_player >= 613)begin //right limit handeling
+//			x_player <= 613;
+//		end
+//		
+//	end
 	
 	always@(posedge clk_1ms)begin
 		case(state)
